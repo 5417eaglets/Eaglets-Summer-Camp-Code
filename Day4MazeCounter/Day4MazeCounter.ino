a49e69dfd4cce50b2d58cb39c9c88d0e177291b8
hannahX4108 <55165083+hannahX4108@users.noreply.github.com>
2021-07-28 19:54:01 -0500

    Add files via upload

---
+++ Day4MazeCounter/Day4MazeCounter.ino
@@ -0,0 +1,101 @@
+#define blueLED 19
+#define redLED 4
+#define Lswitch 14
+
+// —————————————————————————  Motors
+int motorRight[] = {16, 17, 10};
+int motorLeft[] = {11, 12, 9};
+
+int counter = 0;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(redLED, OUTPUT);
+  pinMode(blueLED, OUTPUT);
+
+  pinMode(Lswitch, INPUT);
+  
+  // Setup motors
+  int i;
+  for(i = 0; i < 3; i++){
+    pinMode(motorLeft[i], OUTPUT);
+    pinMode(motorRight[i], OUTPUT);
+  }
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  driveForward(150);
+  if(digitalRead(Lswitch) == HIGH && counter == 0)
+  { 
+    turnLeft();
+    counter++;
+  } 
+  else if(digitalRead(Lswitch) == HIGH && counter == 1)
+  {
+    turnRight();
+    counter++;
+  } 
+  else if(digitalRead(Lswitch) == HIGH && counter == 2)
+  {
+    turnRight();
+    driveForward(150);
+    delay(2000);
+    driveForward(0);
+    delay(100000000);
+    counter++;
+  }
+}
+
+void turnRight(){
+  driveBackward(150);
+  delay(500);
+  driveLeftMotorForward(150);
+  driveRightMotorForward(0);
+  delay(700);
+}
+
+void turnLeft(){
+  driveBackward(150);
+  delay(500);
+  driveRightMotorForward(150);
+  driveLeftMotorForward(0);
+  delay(700);
+}
+
+void driveForward(int power){
+  driveLeftMotorForward(power);
+  driveRightMotorForward(power);
+}
+
+void driveBackward(int power){
+  driveLeftMotorBackward(power);
+  driveRightMotorBackward(power);
+}
+
+// ------------------------------Pre-Made Drive Functions
+void driveLeftMotorForward(int power){
+  analogWrite(motorLeft[2], power);
+  digitalWrite(motorLeft[0], LOW);
+  digitalWrite(motorLeft[1], HIGH);
+}
+
+void driveRightMotorForward(int power){
+  analogWrite(motorRight[2], power);
+  digitalWrite(motorRight[0], LOW);
+  digitalWrite(motorRight[1], HIGH);
+}
+
+void driveLeftMotorBackward(int power){
+  analogWrite(motorLeft[2], power);
+  digitalWrite(motorLeft[0], HIGH);
+  digitalWrite(motorLeft[1], LOW);
+}
+
+void driveRightMotorBackward(int power){
+  analogWrite(motorRight[2], power);
+  digitalWrite(motorRight[0], HIGH);
+  digitalWrite(motorRight[1], LOW); 
+}
